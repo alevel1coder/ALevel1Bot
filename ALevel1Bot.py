aa1e4b993bd6a8dc75aa794b003de9be09139bfe
ALevel1Coder <67092816+alevel1coder@users.noreply.github.com>
2021-06-25 18:56:47 -0400

    Moved from client event based triggers to bot based triggers

--- ALevel1Bot.py
+++ ALevel1Bot.py
@@ -6,51 +6,47 @@ import os
 import random
 import logging
 # Discord imports
-from discord import Client, Message
-from discord.errors import DiscordException
+# from discord.errors import DiscordException
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN=os.getenv('DISCORD_TOKEN')
 logging.basicConfig(level=logging.INFO)
 
-class CustomClient(Client):
-    async def on_ready(self):
-        # This event triggers once the bot is ready and connected
-        logging.info("====================================================")
-        logging.info(f'{self.user} has connected to discord')
+bot = commands.Bot(command_prefix='!')
 
-    async def on_message(self, Message):
-        # This event should trigger when a message is posted in channel the
-        # bot has access to. Currently it responds to specific messages.
-        if Message.author == client.user:
-            return
+@bot.event
+async def on_ready():
+    # This event triggers once the bot is ready and connected
+    logging.info("====================================================")
+    logging.info(f'{bot.user.name} has connected to discord')
 
-        random_responses = [
-                'Bongo',
-                'Banana',
-                'Monke',
-                ]
 
-        if Message.content == 'bingo':
-            response = random.choice(random_responses)
-            await Message.channel.send(response)
-        if Message.content == 'raise-exception':
-            raise DiscordException
+@bot.command(name='yo')
+async def hello(ctx):
+    greetings = [
+            'Salutations',
+            'Sup',
+            'wazaaaap'
+            ]
 
-    async def on_error(self, event, *args, **kwargs):
-        # This event triggers when an error occurs, so we setup the
-        # error handlers here to log the error to a log file.
-        with open('err.log', 'a') as err_file:
-            if event == 'on_message':
-                logging.error(
-                        'Encountered error when reading a message: '
-                        f'{args[0]}'
-                        )
-                err_file.write(f'Unhandled message: {args[0]}\n')
-            else:
-                raise Exception
+    response = random.choice(greetings)
+    await ctx.send(response)
+
+    # Error Handling - See GH-2 for progress
+# async def on_error(self, event, *args, **kwargs):
+#    # This event triggers when an error occurs, so we setup the
+#    # error handlers here to log the error to a log file.
+#    with open('err.log', 'a') as err_file:
+#        if event == 'on_message':
+#            logging.error(
+#                    'Encountered error when reading a message: '
+#                     f'{args[0]}'
+#                     )
+#             err_file.write(f'Unhandled message: {args[0]}\n')
+#         else:
+#             raise Exception
 
 # What actually runs it
-client = CustomClient()
-client.run(TOKEN)
+bot.run(TOKEN)
